fd1f1c7c363a9a3df7434018fda00045c0794215
15sheeps <109234179+15sheeps@users.noreply.github.com>
2025-06-10 19:38:26 +0300

    Update tmux-xfce.sh

--- tmux-xfce.sh
+++ tmux-xfce.sh
@@ -5,7 +5,7 @@ kill -9 $(pgrep -f "termux.x11") 2>/dev/null
 
 # Prepare termux-x11 session
 export XDG_RUNTIME_DIR=${TMPDIR}
-termux-x11 :0 >/dev/null &
+termux-x11 :0 -legacy-drawing >/dev/null &
 
 # Wait a bit until termux-x11 gets started.
 sleep 5
